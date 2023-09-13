96eea5381d499c1c76ef451c292c0d48dc4ae93d
Mitchell Lohmann <144869965+Mitchell-Lohmann@users.noreply.github.com>
2023-09-13 15:07:57 +1000

    Addition of client and server side application for overseer and door

--- Door.c
+++ Door.c
@@ -1,1 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
+void sendMessage(int fd, const char *msg)
+{
+    int len = strlen(msg);
+    uint32_t netLen = htonl(len);
+    send(fd, &netLen, sizeof(netLen), 0);
+    if (send(fd, msg, len, 0) != len) 
+    {
+        fprintf(stderr, "send did not send all data\n");
+        exit(1);
+    }
+}
+
+int main()
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) 
+    {
+        perror("socket()");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    if (inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr) != 1) {
+        perror("inet_pton");
+    }
+
+addr.sin_family = AF_INET;
+addr.sin_port = htons(12345);
+
+// connect
+    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) 
+    {
+        perror("connect()");
+        return 1;
+    }
+
+    char *dataToSend = "123456";
+    sendMessage(fd, dataToSend);
+    sendMessage(fd, dataToSend);
+
+    if (shutdown(fd, SHUT_RDWR) == -1) 
+    {
+        perror("shutdown()");
+        return 1;
+    }
+
+    close(fd);
+}
---
+++ Overseer.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+    if (argc != 2) {
+        printf("\nUsage a.out <portNo>\n");
+        return 1;
+    }
+
+        int bytesRcv;
+        struct sockaddr clientaddr;
+        socklen_t addrlen;
+        char buffer[1024];
+        struct sockaddr_in serverAddress;
+        int fd = socket(AF_INET, SOCK_STREAM, 0); //0 is default for socket
+        implementation
+
+    if (fd==-1) {
+        perror("\nsocket()\n");
+        return 1;
+    }
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(atoi(argv[1]));
+    
+    if (bind(fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress))==-1) 
+    {
+        perror("bind()");
+        return 1;
+        }
+        int clientfd;
+        if (listen(fd, 10)==-1) 
+        {
+            perror("listen()");
+            return 1;
+        }
+
+        while (1) 
+        {
+            clientfd = accept(fd,&clientaddr, &addrlen );
+            if (clientfd==-1) 
+            {
+                perror("accept()");
+                return 1;
+            }
+
+            //1023 so can add null term if req
+            bytesRcv = recv(clientfd, buffer, 1023,0);
+            if (bytesRcv==-1) 
+            {
+                perror("bytesrcv");
+                return 1;
+            }
+
+            buffer[bytesRcv] ='\0';
+            printf("\nNumber of Bytes received from client was %d.\n\nInformation
+            sent through socket --> %s\n\n", bytesRcv, buffer);
+            close(clientfd);
+        }
+
+    if (shutdown(clientfd, SHUT_RDWR) ==-1) 
+    {
+        perror("shutdown()");
+        return 1;
+    }
+
+    close(fd); //sockets can remain open after program termination - when open
+    socket should close it
+}
